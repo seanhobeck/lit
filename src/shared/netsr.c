0a29f93ba47d0863ac82c1ce86e2432ec3803056
sean <schobeck3@gmail.com>
2026-01-07 23:31:43 -0500

    added a netsr.c file for network sending and recv'ing (+2).

---
+++ src/shared/netsr.c
@@ -0,0 +1,5 @@
+/**
+ * @author Sean Hobeck
+ * @date 2026-01-07
+ */
+#include "netsr.h"
---
+++ src/shared/netsr.h
@@ -0,0 +1,8 @@
+/**
+ * @author Sean Hobeck
+ * @date 2026-01-07
+ */
+#ifndef NETSR_H
+#define NETSR_H
+
+#endif /* NETSR_H */
